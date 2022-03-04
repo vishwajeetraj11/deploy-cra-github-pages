a6dd7ca9765eb84d608b89a314caf6b19933e23f
Vishwajeet Raj <vishwajeetraj11@gmail.com>
2022-03-04 22:10:11 +0530

    Deploy react application on github pages.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<div>YO</div>} />
+        <Route path="/damn" element={<div>Damn</div>} />
+        <Route path="/cream" element={<div>Cream</div>} />
+      </Routes>
     </div>
   );
 }
